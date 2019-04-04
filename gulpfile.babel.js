2bb68731b3c9f553401ed0588338246cfdf5b446
Evdokimov Andrey <zoom.flash0@gmail.com>
2019-04-05 00:41:10 +0300

    main menu

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
-import plugins from "gulp-load-plugins";
-import yargs from "yargs";
-import browser from "browser-sync";
-import gulp from "gulp";
-import panini from "panini";
-import rimraf from "rimraf";
-import sherpa from "style-sherpa";
-import yaml from "js-yaml";
-import fs from "fs";
-import webpackStream from "webpack-stream";
-import webpack2 from "webpack";
-import named from "vinyl-named";
-import uncss from "uncss";
-import autoprefixer from "autoprefixer";
+import plugins       from 'gulp-load-plugins';
+import yargs         from 'yargs';
+import browser       from 'browser-sync';
+import gulp          from 'gulp';
+import panini        from 'panini';
+import rimraf        from 'rimraf';
+import sherpa        from 'style-sherpa';
+import yaml          from 'js-yaml';
+import fs            from 'fs';
+import webpackStream from 'webpack-stream';
+import webpack2      from 'webpack';
+import named         from 'vinyl-named';
+import uncss         from 'uncss';
+import autoprefixer  from 'autoprefixer';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
 // Check for --production flag
-const PRODUCTION = !!yargs.argv.production;
+const PRODUCTION = !!(yargs.argv.production);
 
 // Load settings from settings.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
 function loadConfig() {
-  let ymlFile = fs.readFileSync("config.yml", "utf8");
+  let ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
 
 // Build the "dist" folder by running all of the below tasks
 // Sass must be run later so UnCSS can search for used classes in the others assets.
 gulp.task(
-  "build",
+  'build',
   gulp.series(
     clean,
     gulp.parallel(pages, javascript, images, copy),
@@ -42,7 +42,7 @@ gulp.task(
 );
 
 // Build the site, run the server, and watch for file changes
-gulp.task("default", gulp.series("build", server, watch));
+gulp.task('default', gulp.series('build', server, watch));
 
 // Delete the "dist" folder
 // This happens every time a build starts
@@ -53,20 +53,20 @@ function clean(done) {
 // Copy files out of the assets folder
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
 function copy() {
-  return gulp.src(PATHS.assets).pipe(gulp.dest(PATHS.dist + "/assets"));
+  return gulp.src(PATHS.assets).pipe(gulp.dest(`${PATHS.dist}/assets`));
 }
 
 // Copy page templates into finished HTML files
 function pages() {
   return gulp
-    .src("src/pages/**/*.{html,hbs,handlebars}")
+    .src('src/pages/**/*.{html,hbs,handlebars}')
     .pipe(
       panini({
-        root: "src/pages/",
-        layouts: "src/layouts/",
-        partials: "src/partials/",
-        data: "src/data/",
-        helpers: "src/helpers/"
+        root: 'src/pages/',
+        layouts: 'src/layouts/',
+        partials: 'src/partials/',
+        data: 'src/data/',
+        helpers: 'src/helpers/'
       })
     )
     .pipe(gulp.dest(PATHS.dist));
@@ -81,10 +81,10 @@ function resetPages(done) {
 // Generate a style guide from the Markdown content and HTML template in styleguide/
 function styleGuide(done) {
   sherpa(
-    "src/styleguide/index.md",
+    'src/styleguide/index.md',
     {
-      output: PATHS.dist + "/styleguide.html",
-      template: "src/styleguide/template.html"
+      output: `${PATHS.dist}/styleguide.html`,
+      template: 'src/styleguide/template.html'
     },
     done
   );
@@ -102,37 +102,38 @@ function sass() {
   ].filter(Boolean);
 
   return gulp
-    .src("src/assets/scss/app.scss")
+    .src('src/assets/scss/app.scss')
     .pipe($.sourcemaps.init())
     .pipe(
       $.sass({
         includePaths: PATHS.sass
-      }).on("error", $.sass.logError)
+      }).on('error', $.sass.logError)
     )
     .pipe($.postcss(postCssPlugins))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: "ie9" })))
+    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie11' })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + "/assets/css"))
-    .pipe(browser.reload({ stream: true }));
+    .pipe(gulp.dest(`${PATHS.dist}/assets/css`))
+    .pipe(browser.reload({ stream: true })); 
+    //.pipe(browser.stream());
 }
 
-let webpackConfig = {
-  mode: PRODUCTION ? "production" : "development",
+const webpackConfig = {
+  mode: PRODUCTION ? 'production' : 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
             compact: false
           }
         }
       }
     ]
   },
-  devtool: !PRODUCTION && "source-map"
+  devtool: !PRODUCTION && 'source-map'
 };
 
 // Combine JavaScript into one file
@@ -146,24 +147,24 @@ function javascript() {
     .pipe(
       $.if(
         PRODUCTION,
-        $.uglify().on("error", e => {
+        $.uglify().on('error', e => {
           console.log(e);
         })
       )
     )
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + "/assets/js"));
+    .pipe(gulp.dest(`${PATHS.dist}/assets/js`));
 }
 
 // Copy images to the "dist" folder
 // In production, the images are compressed
 function images() {
   return gulp
-    .src("src/assets/img/**/*")
+    .src('src/assets/img/**/*')
     .pipe(
       $.if(PRODUCTION, $.imagemin([$.imagemin.jpegtran({ progressive: true })]))
     )
-    .pipe(gulp.dest(PATHS.dist + "/assets/img"));
+    .pipe(gulp.dest(`${PATHS.dist}/assets/img`));
 }
 
 // Start a server with BrowserSync to preview the site in
@@ -171,8 +172,10 @@ function server(done) {
   browser.init(
     {
       server: PATHS.dist,
-      port: PORT
+      port: PORT,
+      notify: false
     },
+
     done
   );
 }
@@ -187,25 +190,25 @@ function server(done) {
 function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp
-    .watch("src/pages/**/*.html")
-    .on("all", gulp.series(pages, browser.reload));
+    .watch('src/pages/**/*.html')
+    .on('all', gulp.series(pages, browser.reload));
   gulp
-    .watch("src/{layouts,partials}/**/*.html")
-    .on("all", gulp.series(resetPages, pages, browser.reload));
+    .watch('src/{layouts,partials}/**/*.html')
+    .on('all', gulp.series(resetPages, pages, browser.reload));
   gulp
-    .watch("src/data/**/*.{js,json,yml}")
-    .on("all", gulp.series(resetPages, pages, browser.reload));
+    .watch('src/data/**/*.{js,json,yml}')
+    .on('all', gulp.series(resetPages, pages, browser.reload));
   gulp
-    .watch("src/helpers/**/*.js")
-    .on("all", gulp.series(resetPages, pages, browser.reload));
-  gulp.watch("src/assets/scss/**/*.scss").on("all", sass);
+    .watch('src/helpers/**/*.js')
+    .on('all', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
   gulp
-    .watch("src/assets/js/**/*.js")
-    .on("all", gulp.series(javascript, browser.reload));
+    .watch('src/assets/js/**/*.js')
+    .on('all', gulp.series(javascript, browser.reload));
   gulp
-    .watch("src/assets/img/**/*")
-    .on("all", gulp.series(images, browser.reload));
+    .watch('src/assets/img/**/*')
+    .on('all', gulp.series(images, browser.reload));
   gulp
-    .watch("src/styleguide/**")
-    .on("all", gulp.series(styleGuide, browser.reload));
+    .watch('src/styleguide/**')
+    .on('all', gulp.series(styleGuide, browser.reload));
 }
